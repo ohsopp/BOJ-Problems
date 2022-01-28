37ee55a3732ab8b766215262ea316741a5a1d10c
ohsopp <wjdgus0545@gmail.com>
2022-01-28 23:13:22 +0900

    2022.01.28

---
+++ 1406.cpp
@@ -0,0 +1,48 @@
+
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+string s;
+int n;
+
+int main() {
+
+	list<char> l;
+	list<char>::iterator it = l.begin();
+
+	cin >> s >> n;
+
+	int t = s.size();
+
+	for (int i = 0; i < t; i++) l.push_back(s[i]);
+
+	it = l.end();
+	
+	while (n--) {
+		char c;
+
+		cin >> c;
+
+		if (c == 'L') {
+			if (it != l.begin()) it--;
+		}
+		else if (c == 'D') {
+			if (it != l.end()) it++;
+		}
+		else if (c == 'B') {
+			if (it != l.begin()) l.erase(--it);
+		}
+		else if (c == 'P') {
+			char x;
+			cin >> x;
+			l.insert(it, x);
+		}
+	}
+
+	for (it = l.begin(); it != l.end(); it++)
+		cout << *it;
+
+}
