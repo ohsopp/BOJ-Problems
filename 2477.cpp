6d99682b74b07354eecfc842f6ebb0f1c01a34b7
ohsopp <wjdgus0545@naver.com>
2023-02-04 16:58:55 +0900

    Create 2477.cpp

---
+++ 2477.cpp
@@ -0,0 +1,29 @@
+
+// 백준 2477 참외밭
+
+#include <iostream>
+
+using namespace std;
+
+pair<int, int> ary[6];
+int n, maxArea = 1, cur, res;
+
+int main(){
+    
+    cin >> n;
+
+    for (int i = 0; i < 6; i++){
+        cin >> ary[i].first >> ary[i].second;
+    }
+
+    for (int i = 0; i < 6; i++){
+        cur = ary[i].second * ary[(i + 1) % 6].second;
+
+        if (cur > maxArea){
+            maxArea = cur;
+            res = maxArea - ary[(i + 3) % 6].second * ary[(i + 4) % 6].second;
+        }
+    }
+
+    cout << res * n;
+}
